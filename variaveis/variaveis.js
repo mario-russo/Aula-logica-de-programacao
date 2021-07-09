cf11a37aafa332856662b2c8e557a1f7b6cee0af
unknown <mario.russo93@hotmail.com>
2021-07-08 21:43:54 -0300

    aula de variaveis

---
+++ variaveis/variaveis.js
@@ -0,0 +1,25 @@
+//boolean
+var a = true;
+var b = false;
+
+//Number
+var c = 10;
+var d = 11.5;
+
+//String
+var e = "teste";
+
+//Undefined
+var f;
+var g = undefined;
+
+//function
+var a = function(){};
+var b = () => {};
+
+//object
+var c = {};
+var d = []; //array
+var e = null;
+//variaveis que não muda
+const pi = 3.1415;
